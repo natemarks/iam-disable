2048b5d02cb6e77fd05c85205d8e5b9dbf7e3f96
Nate Marks <nmarks@imprivata.com>
2023-11-09 08:52:28 -0500

    Add iam-disable-date  tag to disabled accounts
    This makes it easier to figure out which accounts got disable and when so we cna go back and delete them

--- disable/main.go
+++ disable/main.go
@@ -2,6 +2,7 @@ package disable
 
 import (
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -9,6 +10,10 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/iam/types"
 	"github.com/natemarks/iam-disable/discover"
 	"github.com/rs/zerolog"
+)
+
+const (
+	tagKey = "iam-disabled-date"
 )
 
 func disableIAMUserConsolePassword(username string) error {
@@ -55,10 +60,69 @@ func disableAccessKey(accessKeyID string, userName string) error {
 	return err
 }
 
+func Today() string {
+	// Get the current time in the local timezone
+	currentTime := time.Now()
+
+	// Format the current time as a string in the "2006-01-02" layout
+	dateString := currentTime.Format("2006-01-02")
+
+	return dateString
+}
+func TagIamUser(username string) (err error) {
+	// Load your AWS credentials and configuration from a shared credentials file
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		panic("configuration error, " + err.Error())
+	}
+	client := iam.NewFromConfig(cfg)
+
+	// List the tags for the IAM user
+	listTagsOutput, err := client.ListUserTags(ctx, &iam.ListUserTagsInput{
+		UserName: aws.String(username),
+	})
+	if err != nil {
+		return err
+	}
+
+	// Check if the tag key already exists
+	tagExists := false
+	for _, tag := range listTagsOutput.Tags {
+		if *tag.Key == tagKey {
+			tagExists = true
+			break
+		}
+	}
+
+	// If the tag key doesn't exist, create the tag
+	if !tagExists {
+		_, err := client.TagUser(ctx, &iam.TagUserInput{
+			UserName: aws.String(username),
+			Tags: []types.Tag{
+				{
+					Key:   aws.String(tagKey),
+					Value: aws.String(Today()),
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
 // IamUser disables an IAM user
 func IamUser(username string, log *zerolog.Logger) {
 	userLog := log.With().Str("username", username).Logger()
-	err := disableIAMUserConsolePassword(username)
+	err := TagIamUser(username)
+	if err != nil {
+		userLog.Error().Msgf("failed to tag user %s, %s", username, err)
+	}
+	userLog.Info().Msgf("disabling user %s", username)
+	err = disableIAMUserConsolePassword(username)
 	if err != nil {
 		userLog.Error().Msgf("failed to disable console password for user %s, %s", username, err)
 	} else {
